6dd97b9dc671d62c5b63de0089dc526986a251d7
Steven <Briggs.steven369@gmail.com>
2023-02-11 13:54:47 -0800

    Implement login, signup, and local-storage

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,23 @@
 import {useEffect, useState} from "react";
 import { MovieCard } from "../movie-card/movie-card"
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import {SignupView} from "../signup-view/signup-view";
 
 export const MainView = () => {
   	const [movies, setMovies] = useState([]);
-
-const [selectedMovie, setSelectedMovie] = useState(null);
+	const [selectedMovie, setSelectedMovie] = useState(null);
+	const [user, setUser] = useState(null)
+	const [token, setToken] = useState(null);
+	const storedUser = JSON.parse(localStorage.getItem("user"));
+	const storedToken = JSON.parse(localStorage.getItem("token"));
 
 	useEffect(() => {
-		fetch("https://mymov-project.herokuapp.com/movies")
+		if (!token) return;
+
+		fetch("https://mymov-project.herokuapp.com/movies", {
+			headers: { Authorization: 'Bearer ${token}' }
+		})
 			.then((response) => response.json())
 			.then((data) => {
 			  const moviesFromApi = data.map((movie) => { return {
@@ -18,33 +27,52 @@ const [selectedMovie, setSelectedMovie] = useState(null);
 				  image: movie.ImageURL,
 				  genre: movie.genre,
 				  director: movie.Director,
-			  };
+			  }
 			  });
+			  setMovies(moviesFromApi);
+			});
+	  }, [token])
 
-			setMovies(moviesFromApi);
-		  });
-	  }, []);
+	  if (!user) {
+    return (
+		<>
+			<LoginView onLoggedIn={(user, token) => {
+				setUser(user);
+				setToken(token);
+			}} />
+			or
+			<SignupView />
+		</>
+    );
+  }
 
-if (selectedMovie) {
-	return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
-}
+	if (selectedMovie) {
+		return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
+	}
 
-if (movies.length === 0) {
-	return <div>The list is empty!</div>
-}
+	if (movies.length === 0) {
+		return (
+			<div>
+				<button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+				<div>The list is empty!</div>
+			</div>
 
-return (
-	<div>
-		{movies.map((movie) => (
-			<MovieCard
-				key={movie.id}
-				movie={movie}
-				onMovieClick={(newSelectedMovie) => {
-					setSelectedMovie(newSelectedMovie);
-				}}
-			/>
-		))}
-	</div>
-);
+		);
+	}
+
+	return (
+		<div>
+			{movies.map((movie) => (
+				<MovieCard
+					key={movie.id}
+					movie={movie}
+					onMovieClick={(newSelectedMovie) => {
+						setSelectedMovie(newSelectedMovie);
+					}}
+				/>
+			))}
+			<button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+		</div>
+	);
 };
 

38ddaf705120575adff8d1bd38d7a7e689cbb3ac
Steven <Briggs.steven369@gmail.com>
2023-02-17 15:13:10 -0800

    implement Ebere's suggestions

--- src/components/favorite-movies-view/favorite-movies-view.jsx
+++ src/components/favorite-movies-view/favorite-movies-view.jsx
@@ -15,20 +15,18 @@ export const FavoriteMoviesView = ({favMovies}) => {
         }
     });
 
+    if (favoriteMovies.length === 0) {
+        return <Col>The list of favorite movies is empty</Col>
+    }
+
     return (
-        <Row>
-            {favoriteMovies.length === 0 ? (
-                <Col>The list of favorite movies is empty</Col>
-            ) : (
-                <>
-                    <div className='text-start h2 mb-4'>List of favorite movies</div>
-                    {favoriteMovies.map((movie) => (
-                        <Col className="mb-4" key={movie.id} md={3}>
-                            <MovieCard movie={movie}/>
-                        </Col>
-                    ))}
-                </>
-            )}
-        </Row>
-    );
+        <>
+            <div className='text-start h2 mb-4'>List of favorite movies</div>
+            {favoriteMovies.map((movie) => (
+                <Col className="mb-4" key={movie.id} md={3}>
+                    <MovieCard movie={movie}/>
+                </Col>
+            ))}
+        </>
+    )
 };
